4d1f7dae898d45e42042cb3c8b85ad29c2244a1d
Maik Strewe <mstrewe@googlemail.com>
2015-11-02 21:55:32 +0100

    add basic server for tag id db

---
+++ server/iddb.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/getroombytagid/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "rooms.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       var room = data[req.params.id] 
+       console.log( room );
+       res.end( JSON.stringify(room));
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
